d07c156f2d85d9fa624c5c3e218ec36b16fe0725
Ishika sharma <84780456+Ishikaa27@users.noreply.github.com>
2021-10-26 11:00:36 +0530

    Add files via upload

--- Chart.py
+++ Chart.py
@@ -3,14 +3,15 @@ from datetime import date
 
 from matplotlib import pyplot as plt
 import yfinance as yf
-# from fbprophet import Prophet
-# from fbprophet.plot import plot_plotly
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
+
 
 START = "2016-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 st.title("AIR Trading")
-stocks = ("NVIDIA Corporation","Facebook Inc.","Uber Technologies Inc.","Microsoft Corporation","Apple Inc.","Tesla Inc.","Pfizer Inc.","Ford Motor Company","The Coca-Cola Company","SoFi Technologies Inc.")
+stocks = ('IBM', 'MSFT', 'AAPL', 'AMZN','TSLA','INTC','PINS','FB','NOK','PSFE')
 selected_stocks = st.selectbox("Stock Company", stocks)
 n_years = st.slider("Years of prediction:",1,4)
 period = n_years* 365
@@ -32,7 +33,8 @@ def plot_raw_data():
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
-    fig.layout.update(text_title="Time Series Data", xaxis_rangeslider_visible=True)
+    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
 
 plot_raw_data()
+
