3d0ffcb7db932048e782ec6a3c50eaf144839630
Ishika sharma <84780456+Ishikaa27@users.noreply.github.com>
2021-11-18 08:41:21 +0530

    Add files via upload

---
+++ Chart.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from datetime import date
+
+from matplotlib import pyplot as plt
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
+
+
+START = "2016-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+st.title("AIR Trading")
+stocks = ('IBM', 'MSFT', 'AAPL', 'AMZN','TSLA','INTC','PINS','FB','NOK','PSFE')
+selected_stocks = st.selectbox("Stock Company", stocks)
+n_years = st.slider("Years of prediction:",1,4)
+period = n_years* 365
+
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker,START,TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+data_load_state = st.text("Load data...")
+data = load_data(selected_stocks)
+data_load_state.text("Loading data...done!")
+
+st.subheader('Raw Data')
+st.write(data.tail())
+
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
+    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+plot_raw_data()
+
